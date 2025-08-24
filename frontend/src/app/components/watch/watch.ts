a6e1c998ebb5fb3634d2b49d196a901ad9e6cf1f
Mohamed <mohamedtarekk710@gmail.com>
2025-08-24 03:24:44 +0300

    very Small changes

--- frontend/src/app/components/watch/watch.ts
+++ frontend/src/app/components/watch/watch.ts
@@ -5,12 +5,26 @@ import { Watchlist } from '../../services/watchlist/watchlist';
   selector: 'app-watch',
   imports: [],
   templateUrl: './watch.html',
-  styleUrl: './watch.css'
+  styleUrl: './watch.css',
 })
 export class Watch implements OnInit {
-  movies:any[]=[];
-  constructor(private allWatchList:Watchlist){}
+  movies: any[] = [];
+  constructor(private allWatchList: Watchlist) {}
   ngOnInit(): void {
-    this.movies=this.allWatchList.watchlist;
+    this.allWatchList.getWatchlist().subscribe({
+      next: (data) => {
+        this.movies = data;
+        console.log(this.movies)
+      },
+      error: (err) => {
+        console.error('Error fetching watchlist:', err);
+      },
+      complete: () => {
+        console.log('Watchlist fetch complete');
+      },
+    });
+  }
+  removeMovie(id: number) {
+    this.movies = this.movies.filter(movie => movie._id !== id);
   }
 }
--- frontend/src/app/services/watchlist/watchlist.ts
+++ frontend/src/app/services/watchlist/watchlist.ts
@@ -5,60 +5,24 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class Watchlist {
-  //dummy
-  watchlist:any[]=[
-  {
-    "movieId": "tt0111161",
-    "title": "The Shawshank Redemption",
-    "poster": "https://image.tmdb.org/t/p/w500/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
-    "rating": 9.3,
-    "description": "Two imprisoned men form a deep friendship over the years, finding solace and redemption through acts of common decency."
-  },
-  {
-    "movieId": "tt0068646",
-    "title": "The Godfather",
-    "poster": "https://image.tmdb.org/t/p/w500/3bhkrj58Vtu7enYsRolD1fZdja1.jpg",
-    "rating": 9.2,
-    "description": "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son."
-  },
-  {
-    "movieId": "tt0468569",
-    "title": "The Dark Knight",
-    "poster": "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
-    "rating": 9.0,
-    "description": "Batman sets out to dismantle organized crime in Gotham, but finds himself facing the anarchic mastermind known as the Joker."
-  },
-  {
-    "movieId": "tt0109830",
-    "title": "Forrest Gump",
-    "poster": "https://image.tmdb.org/t/p/w500/saHP97rTPS5eLmrLQEcANmKrsFl.jpg",
-    "rating": 8.8,
-    "description": "The story of a kind-hearted man with a low IQ who unintentionally influences several defining events in American history."
-  },
-  {
-    "movieId": "tt0133093",
-    "title": "The Matrix",
-    "poster": "https://image.tmdb.org/t/p/w500/f89U3ADr1oiB1s9GkdPOEpXUk5H.jpg",
-    "rating": 8.7,
-    "description": "A computer hacker discovers that his world is a simulated reality and joins a rebellion against its controllers."
-  }
-]
 
-  private apiwatchlist = 'http://localhost:5000/api/watchlist' 
+private apiwatchlist = 'http://localhost:3000/watchlist';
   //url el watchlist
   constructor(private http: HttpClient){}
 
-  getWatchList(): Observable<any> { // show all
+  getWatchlist(): Observable<any> { // show all
     return this.http.get<any>(this.apiwatchlist);
+
   }
 
   //add one
-  addtowatchlist(movieId:number): Observable<any> {
+  addToWatchlist(movieId:number): Observable<any> {
     return this.http.post(`${this.apiwatchlist}/add`, {movieId})
+
   }
 
   //remove
-  RemoveFromWatchList(movieId:number):Observable<any>{
-    return this.http.delete(`${this.apiwatchlist}/remove/${movieId}`)
+  removeFromWatchlist(movieId:number):Observable<any>{
+    return this.http.delete(`${this.apiwatchlist}/watchlist/${movieId}`)
   }
 }
