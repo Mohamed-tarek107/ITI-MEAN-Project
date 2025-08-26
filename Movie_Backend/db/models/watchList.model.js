de8764ccbbe839b0fced60749a0f3fb99efba12f
mazen <mazenhaytham26@gmail.com>
2025-08-26 04:39:44 +0300

    edit the schema of watchlist

--- Movie_Backend/db/models/watchList.model.js
+++ Movie_Backend/db/models/watchList.model.js
@@ -1,13 +1,22 @@
 import mongoose from "mongoose";
 
-
 const watchListSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  poster: { type: String, required: true},
-  addedAt: { type: Date, default: Date.now },
-  description: { type: String, required: true},
-  rating: { type: String, required: true}
-},{versionKey: false});
-
+  adult: Boolean,
+  backdrop_path: String,
+  genre_ids: [Number],
+  id: Number,
+  original_language: String,
+  original_title: String,
+  overview: String,
+  popularity: Number,
+  poster_path: String,
+  release_date: String,
+  title: String,
+  video: Boolean,
+  vote_average: Number,
+  vote_count: Number,
+  isLiked: { type: Boolean, default: false }
+}, { versionKey: false });
 
 export const Watch = mongoose.model('WatchList', watchListSchema);
+
